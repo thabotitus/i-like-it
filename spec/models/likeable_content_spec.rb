14856dac407638096625457951d8c5c9bf8bd342
Thabo Titus <hello@thabotitus.co.za>
2017-01-06 12:41:30 +0200

    basic user functions

--- spec/models/likeable_content_spec.rb
+++ spec/models/likeable_content_spec.rb
@@ -1,25 +1,28 @@
 require 'rails_helper'
 
-describe LikeableContent do
+describe LikeableContent, type: :model do
   it 'has a title' do
     content = LikeableContent.new(title: '')
     expect(content.valid?).to eq(false)
   end
 
   it 'does not need a description' do
-    content = LikeableContent.new(title: 'card', description: '', identifier: "string")
+    user = User.create(valid_user)
+    content = LikeableContent.new(title: 'card', description: '', identifier: "string", user: user)
     expect(content.valid?).to eq(true)
   end
 
   context '#identifier' do
     it 'has a unique identifier' do
-      content = LikeableContent.create(title: 'card')
+      user = User.create(valid_user)
+      content = LikeableContent.create(title: 'card', user: user)
       expect(content.valid?).to eq(true)
     end
 
     it 'has a string value' do
       stub_unique_identifier
-      content = LikeableContent.create(title: 'card')
+      user = User.create(valid_user)
+      content = LikeableContent.create(title: 'card', user: user)
       expect(content.identifier).to eq('unique-indentifier')
     end
   end
@@ -28,5 +31,14 @@ describe LikeableContent do
 
   def stub_unique_identifier
     allow_any_instance_of(LikeableContent).to receive(:generate_uuid).and_return('unique-indentifier')
+  end
+
+  def valid_user(attr = {})
+    {
+      first_name: 'John',
+      last_name: 'Cena',
+      email: 'john.cena@ilikeit.co.za',
+      password: 'ucantseeme!'
+    }.merge(attr)
   end
 end
